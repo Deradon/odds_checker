a1796cdad8a088d3ec6b562256bd32ae66caf596
Patrick Helm <me@patrick-helm.de>
2016-06-06 00:42:58 +0200

    Adds `Score#tendency`

--- lib/odds_checker/score.rb
+++ lib/odds_checker/score.rb
@@ -11,6 +11,13 @@ module OddsChecker
       team_one_goals - team_two_goals
     end
 
+    def tendency
+      return :win if difference > 0
+      return :loss if difference < 0
+
+      return :draw
+    end
+
     def ==(other_score)
       other_score.team_one_goals == team_one_goals && other_score.team_two_goals == team_two_goals
     end
--- spec/odds_checker/score_spec.rb
+++ spec/odds_checker/score_spec.rb
@@ -1,14 +1,42 @@
 require 'spec_helper'
 
 describe OddsChecker::Score do
-  subject {
+  subject(:score) {
     described_class.new(
-      team_one_goals: 2,
-      team_two_goals: 1
+      team_one_goals: team_one_goals,
+      team_two_goals: team_two_goals
     )
   }
+
+  let(:team_one_goals) { 2 }
+  let(:team_two_goals) { 1 }
 
   its(:team_one_goals) { is_expected.to eq(2) }
   its(:team_two_goals) { is_expected.to eq(1) }
   its(:difference) { is_expected.to eq(1) }
+
+  describe "#tendency" do
+    subject { score.tendency }
+
+    context "when team one has more goals than team two" do
+      let(:team_one_goals) { 2 }
+      let(:team_two_goals) { 1 }
+
+      it { is_expected.to eq(:win) }
+    end
+
+    context "when team one has less goals than team two" do
+      let(:team_one_goals) { 1 }
+      let(:team_two_goals) { 2 }
+
+      it { is_expected.to eq(:loss) }
+    end
+
+    context "when team one has same goals as team two" do
+      let(:team_one_goals) { 2 }
+      let(:team_two_goals) { 2 }
+
+      it { is_expected.to eq(:draw) }
+    end
+  end
 end
